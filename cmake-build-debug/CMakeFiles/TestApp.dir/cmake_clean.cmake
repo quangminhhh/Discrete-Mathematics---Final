c552f89fd448eeadda224e5b542811b02ab6cf21
DUONG QUANG MINH <93139158+0828500939@users.noreply.github.com>
2024-11-19 00:53:23 +0700

    Bus Router

---
+++ cmake-build-debug/CMakeFiles/TestApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestApp.dir/ncurses/test.cpp.obj"
+  "CMakeFiles/TestApp.dir/ncurses/test.cpp.obj.d"
+  "TestApp.exe"
+  "TestApp.exe.manifest"
+  "TestApp.pdb"
+  "libTestApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TestApp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TestApp.
+# This may be replaced when dependencies are built.
